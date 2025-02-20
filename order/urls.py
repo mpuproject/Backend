fe294f66f555d41e863a1c0d8feb2977da63cc34
JackyWaa <2366171124@qq.com>
2025-02-20 16:25:32 +0800

    update

--- order/urls.py
+++ order/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.get_order_view, name='getOrderById'),
     path('update/', views.update_order_view, name='updateOrder'),
     path('list/', views.get_order_by_user_id_view, name='getOrderByUserId'),
-    path('item/update/', views.update_order_item_view, name='updateOrderItem'),
+    path('item/update/', views.update_order_item_view, name='updateOrderItem'), 
 ]
